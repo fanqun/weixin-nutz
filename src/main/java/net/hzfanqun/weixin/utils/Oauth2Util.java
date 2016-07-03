92ba3bef0a71dab1b01031a15e5e828b8a16b919
fineliving <hzfanqun@gmail.com>
2016-07-04 06:29:10 +0800

    update

---
+++ src/main/java/net/hzfanqun/weixin/utils/Oauth2Util.java
@@ -0,0 +1,20 @@
+package net.hzfanqun.weixin.utils;
+
+import java.io.UnsupportedEncodingException;
+
+public class Oauth2Util {
+
+    public static String urlEncodeUTF8(String source) {
+        String result = source;
+        
+        try {
+            result = java.net.URLEncoder.encode(source,"utf-8");
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    
+    
+ }
