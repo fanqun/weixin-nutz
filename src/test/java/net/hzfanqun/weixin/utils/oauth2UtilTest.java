f4894c55f55d1f5c93944c51f91178af6b508ce5
fanqun <hzfanqun@gmail.com>
2016-08-06 13:57:09 +0800

    Merge branch 'master' of https://github.com/fanqun/weixin-nutz
    # Conflicts:
    #	.gitignore
    #	.settings/org.eclipse.wst.common.component
    #	pom.xml
    #	src/main/java/net/hzfanqun/weixin/bean/HBWxUser.java
    #	src/main/java/net/hzfanqun/weixin/modules/WxModule.java
    #	src/main/java/net/hzfanqun/weixin/utils/Oauth2Util.java
    #	src/main/resources/config/datasource-hb.js
    #	src/main/resources/custom/datasource-hb.properties
    #	src/main/resources/custom/datasource.properties
    #	src/main/resources/custom/wx.properties
    #	src/main/webapp/WEB-INF/web.xml

--- src/test/java/net/hzfanqun/weixin/utils/oauth2UtilTest.java
+++ src/test/java/net/hzfanqun/weixin/utils/oauth2UtilTest.java
@@ -5,10 +5,12 @@ import org.junit.Test;
 /**
  * Created by fanqun on 2016/7/4.
  */
-public class oauth2UtilTest {
+public class Oauth2UtilTest {
+
     @Test
     public void urlEncodeUTF8Test(){
         String oauthUrl = "http://fineliving.ngrok.wendal.cn/hb/oauth/service";
         System.out.println(Oauth2Util.urlEncodeUTF8(oauthUrl));
     }
+
 }
